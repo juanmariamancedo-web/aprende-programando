95cdb58237f72d756a8cce9eae6da06b95057e54
Juanma <juanmaninc@gmail.com>
2023-03-16 12:21:48 -0300

    Termine el trabajo

--- main.js
+++ main.js
@@ -1,17 +1,33 @@
-document.querySelectorAll(`a[href^="#"]`).forEach(anchor =>{
-    console.log(anchor)
-    anchor.addEventListener("click", (e)=>{
-        e.preventDefault();
+//Programamos las navegaciones internas
+const links = document.querySelectorAll('.nav-link');
 
-        const target = document.querySelector(this.getAttribute("href"));
+console.log(links)
 
-        const topOffset = target.offsetTop + 64;
+links.forEach(link => {
+  //Añadimos eventos de click los anchors con id en href
+  link.addEventListener('click', (event) => {
+    event.preventDefault();
 
-        window.scrollTo({
-            top: topOffset,
+    //Obtenemos el target
+    const section = link.getAttribute('href');
+    const target = document.getElementById(section.slice(section.indexOf("#") + 1))
+
+    //Si target es null, hacemos scroll a top: 0
+    if(!target){
+        return window.scrollTo({
+            top: 0,
             behavior: "smooth"
-        })
+        });    
+    }
 
-        console.log("Hay click")
-    })
-})
+    const topOffset = target.offsetTop - 64;
+
+    console.log(target.offsetTop - 100)
+
+    window.scrollTo({
+      top: topOffset,
+      behavior: "smooth"
+    });
+
+  });
+});
